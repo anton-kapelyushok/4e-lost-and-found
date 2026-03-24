7a5ab97f3459219dbdc2f8391b8cc654e3211202
Anton Kapeliushok <Anton.Kapeliushok@jetbrains.com>
2026-03-24 11:52:52 +0100

    initial

---
+++ src/main/kotlin/fe/lnf/FeLnfApplication.kt
@@ -0,0 +1,11 @@
+package fe.lnf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeLnfApplication
+
+fun main(args: Array<String>) {
+    runApplication<FeLnfApplication>(*args)
+}
---
+++ src/test/kotlin/fe/lnf/FeLnfApplicationTests.kt
@@ -0,0 +1,13 @@
+package fe.lnf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeLnfApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
